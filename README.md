f96d753e8d1ccfed93a255f24a2cd9c114c6520e
Gaolei He <hegaolei@henu.edu.cn>
2025-07-12 18:45:55 +0800

    first version

---
+++ README.md
@@ -0,0 +1,32 @@
+# linux上使用clash
+
+## 安装
+
+'''bash
+git clone https://github.com/gaolei-he/clash_for_linux_server.git
+cd clash_for_linux_server
+./install.sh
+'''
+
+并将你的配置文件放在`~/.config/clash/config.yaml`
+
+## 启动
+
+```bash
+start_clash
+```
+
+## 退出
+
+1. 使用`Ctrl+D`退出终端，clash会自动退出
+2. 使用`exit_clash`命令退出
+
+## 细节
+
+clash默认代理端口为7890，如需更改，修改`install.sh`中的所有`7890`为你需要的端口。
+
+默认情况下，安装脚本会将`clash`的配置文件和程序放在`~/.config/clash/`
+
+输出log到`~/.config/clash/clash.log`
+
+安装后可删除该仓库。
---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+mkdir -p ~/.config/clash
+mv ./data/Country.mmdb ./data/cfw ~/.config/clash/
+echo '
+# ==== clash config start ====
+start_clash() {
+    export http_proxy='http://127.0.0.1:7890'
+    export https_proxy='http://127.0.0.1:7890'
+    export HTTP_PROXY='http://127.0.0.1:7890'
+    export HTTPS_PROXY='http://127.0.0.1:7890'
+    ~/.config/clash/cfw >~/.config/clash/clash.log 2>&1 &
+    echo "Clash started. Log file: ~/.config/clash/clash.log"
+
+    export CLASH_PID=$!
+    trap "echo \"exit clash\"; kill -9 $CLASH_PID; exit" EXIT
+}
+exit_clash() {
+    if [ -n "$CLASH_PID" ]; then
+        kill -9 $CLASH_PID
+        echo "Clash stopped"
+        unset CLASH_PID
+        unset http_proxy
+        unset https_proxy
+        unset HTTP_PROXY
+        unset HTTPS_PROXY
+    else
+        echo "Clash is not running."
+    fi
+}
+# ==== clash config end ====
+' >> ~/.bashrc
+
+source ~/.bashrc
